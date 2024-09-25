90b0be698fde7e9fbf18fddf3d596727a25e539b
yousra elmaghraby <yousraelmaghraby@macbook-pro.home>
2024-09-25 11:23:28 +0200

    done

--- weather/cypress.config.cjs
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-     baseUrl: 'http://localhost:5173', // Update this to your actual React app's URL
-     supportFile: false,
-    },
-});
